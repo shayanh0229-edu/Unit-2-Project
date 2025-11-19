4dd3b93f8b9dc35b6f2fdbc9c047e87f0f57f6d6
ShayanHoque <email@email>
2025-11-19 14:47:57 -0500

    d

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,49 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Locale;
 import java.util.Scanner;
 
-
 public class Main {
-   public static void main(String[] args) {
-       Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println();
+        System.out.println("Welcome to the Stock Analyzer. The below are commands you must use (Non-Case Sensitive)");
+        System.out.println();
+        System.out.println("Input a publicly traded company: createStock, (Ticker Symbol), (Market Cap in an Integer), (Number of Outstanding Shares)");
+        System.out.println("Example: createStock, NVDA, 100000, 100");
+        System.out.println();
+        System.out.println("Show Stock Fluctuations over every (Integer n) hours: stockFluctuations, (Ticker Symbol), (Int n)");
+        System.out.println("Example: stockFluctuations, NVDA, 2");
+        System.out.println();
+        System.out.println("In, (Ticker Symbol), (Market Cap in an Integer), (Number of Outstanding Shares)");
+;
+
+        System.out.println("Show Stock Data: stockFluctuations, (Ticker Symbol)");
+        System.out.println("Example: stockFluctuations, NVDA, 2");
+
+        String creatorCommand = "createstock"; // the command variables are in lowercase in order to prevent case sensitivity
+        String fluctuationsCommand = "stockfluctuations";
+        String input = scan.nextLine().toLowerCase();
+        if (input.equals(creatorCommand)) {
+            System.out.println("What is the name of your stock?");
+            String name = scan.nextLine();
+            System.out.println("What is the Market Cap of this company?");
+            int marketCap = scan.nextInt();
+            System.out.println("How many shares are there?");
+            int shares = scan.nextInt();
+            StockCreator stock = new StockCreator(name, marketCap, shares);
 
 
-       System.out.println("Welcome to the Stock Analyzer. The below are commands you must use (Non-Case Sensitive");
+        } else if (input.equals(fluctuationsCommand)) {
+            System.out
+        }
 
 
-       System.out.println("Input a publicly traded company: createStock, (Ticker Symbol), (Market Cap in an Integer), (Number of Outstanding Shares)");
-       System.out.println("Example: createStock, NVDA, 100000, 100");
 
 
-       System.out.println("Show Stock Fluctuations over every hour of the market: stockFluctuations, (Ticker Symbol), (Int n)");
-       System.out.println("Example: stockFluctuations, NVDA, 2");
-//
-//        System.out.println("In, (Ticker Symbol), (Market Cap in an Integer), (Number of Outstanding Shares)");
-       System.out.println("Example: createStock, NVDA, 100000, 100");
 
 
-       System.out.println("Show Stock Data: stockFluctuations, (Ticker Symbol)");
-       System.out.println("Example: stockFluctuations, NVDA, 2");
+        }
 
-
-    
-
-
-       }
-       }
- 
+    }
--- src/StockCreator.java
+++ src/StockCreator.java
@@ -1,16 +1,44 @@
 public class StockCreator {
+
     private String stockName;
-    private int stockMarketCap;
-    private int stockShares;
+    private int marketCapitalization;
+    private int numberOfShares;
+
+    public StockCreator(String name, int marketCap, int shares)
+    {
+        stockName = name;
+        marketCapitalization = marketCap;
+        numberOfShares = shares;
+    }
+    public void displayStock() {
 
 
-    public StockCreator(String stockTicker, int MarketCap, int quantityShares) {
-        stockTicker = stockName;
-        stockMarketCap = MarketCap; // lines 9 and 10; the variables must be swapped
-        stockShares = quantityShares;
+
+    }
+    public void stockFluctuations()
+    {
+        System.out.println("Value of a share of " + stockName + " over the hours of the market: ");
+        int shareValue = marketCapitalization/numberOfShares;
+        int fluctuation = 0;
+
+        for (int hour = 9; hour <= 14; hour++)
+        {
+            fluctuation = (int)(Math.random() * (shareValue * 0.05 )) - 5;
+            shareValue += fluctuation;
+            if (hour < 12)
+                System.out.println(hour + "AM: " + shareValue);
+            else
+                System.out.println(hour + "PM: " + shareValue);
+        }
     }
 
-    public void stockFluctuations(stockTicker) { //Need to call a stock object using this method
 
+    public void invest(int shares, double currentValue)
+    {
+        marketCapitalization += shares * currentValue;
+        // Updates market capitalization based on investment
+        // Since I used Market Cap in writing the share price in other methods,
+        // this is all I need to do to update the numbers everywhere
     }
+
 }
