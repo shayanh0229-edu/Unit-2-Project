96f46096ed218a48b04f5715b23ee9ac353f68f7
shayanh0229-edu <shayanh@nycstudents.net>
2025-11-24 14:50:14 -0500

    end of class 11/24

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,12 @@ public class Main {
 
         System.out.println();
         System.out.println("Welcome to the Stock Analyzer. The below are commands you must use (Non-Case Sensitive)");
+        System.out.println("In your simulated stock market, you may make up to 5 companies with indices from 1 to 5");
         System.out.println();
-        System.out.println(" 1: Input a publicly traded company: createStock,(Ticker Symbol),(Market Cap in an Integer),(Number of Outstanding Shares)");
-        System.out.println("Example: 1, NVDA, 100000, 100");
+        System.out.println(" 1: Input a publicly traded company: 1,(Ticker Symbol),(Market Cap in an Integer),(Number of Outstanding Shares),(IndexNumber)");
+        System.out.println("Example: 1, NVDA, 100000,555,1");
         System.out.println();
-        System.out.println("Show Stock Fluctuations over every hour: stockFluctuations,(Ticker Symbol)");
+        System.out.println(" 2: Show Stock Fluctuations over every hour: stockFluctuations,(Ticker Symbol)");
         System.out.println("Example: 2, NVDA");
         System.out.println();
         System.out.println("In, (Ticker Symbol), (Market Cap in an Integer), (Number of Outstanding Shares)");
@@ -20,20 +21,37 @@ public class Main {
         String creatorCommand = "CREATESTOCK"; // the command variables are in uppercase in order to prevent case sensitivity
         String fluctuationsCommand = "STOCKFLUCTUATIONS"; //also makes the ticker symbols uppercase        
 
-        while (true) { 
+        while (true) {
             String input = scan.nextLine().toUpperCase();
-        if (input.split(",")[0].equals("1")) {  
-            Class stock = new Class(input.split(",")[1], Integer.parseInt(input.split(",")[2]), Integer.parseInt(input.split(",")[3]));
-            stock.displayStock();
-            scan.nextLine();
+            if (input.split(",")[0].equals("1")) {
+                if (Integer.parseInt(input.split(",")[3]) == 1) {
+                    StockCreator one = new StockCreator(input.split(",")[1], Integer.parseInt(input.split(",")[2]), Integer.parseInt(input.split(",")[3]), Integer.parseInt(input.split(",")[3]));
+                    one.displayStock();
+                    scan.nextLine();
+                } else if (Integer.parseInt(input.split(",")[3]) == 2) {
+                    StockCreator two = new StockCreator(input.split(",")[1], Integer.parseInt(input.split(",")[2]), Integer.parseInt(input.split(",")[3]), Integer.parseInt(input.split(",")[3]));
+                    two.displayStock();
+                    scan.nextLine();
+                } else if (Integer.parseInt(input.split(",")[3]) == 3) {
+                    StockCreator three = new StockCreator(input.split(",")[1], Integer.parseInt(input.split(",")[2]), Integer.parseInt(input.split(",")[3]), Integer.parseInt(input.split(",")[3]));
+                    three.displayStock();
+                    scan.nextLine();
+                } else if (Integer.parseInt(input.split(",")[3]) == 4) {
+                    StockCreator four = new StockCreator(input.split(",")[1], Integer.parseInt(input.split(",")[2]), Integer.parseInt(input.split(",")[3]), Integer.parseInt(input.split(",")[3]));
+                    four.displayStock();
+                    scan.nextLine();
+                } else if (Integer.parseInt(input.split(",")[3]) == 5) {
+                    StockCreator five = new StockCreator(input.split(",")[1], Integer.parseInt(input.split(",")[2]), Integer.parseInt(input.split(",")[3]), Integer.parseInt(input.split(",")[3]));
+                    five.displayStock();
+                    scan.nextLine();
+                }
+            } else if (input.split(",")[0].equals("2")) {
+                System.out.println(input.split(",")[1]);
+                scan.nextLine();
 
-        } else if (input.split(",")[0].equals("2")) {
-            System.out.println(input.split(",")[1]);
-            scan.nextLine();
+            }
+
 
         }
-
-
     }
-    }
-    }
+}
